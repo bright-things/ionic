b95e1d1e2a155421c6f54ba52b90257199d1037f
Lars Boegild Thomsen <lth@netcompartner.com>
2015-04-21 14:08:52 +0800

    Testing

--- target/linux/ar71xx/generic/profiles/minibox.mk
+++ target/linux/ar71xx/generic/profiles/minibox.mk
@@ -5,13 +5,13 @@
 # See /LICENSE for more information.
 #
 
-define Profile/minibox1
-	NAME:=minibox1
+define Profile/Minibox1
+	NAME:=Minibox1
 	PACKAGES:=kmod-ath9k kmod-usb-core kmod-usb-ohci kmod-usb2 kmod-ledtrig-usbdev
 endef
 
-define Profile/minibox1/Description
+define Profile/Minibox1/Description
 	Package set optimized for the Minibox v1.0
 endef
 
-$(eval $(call Profile,minibox1))
+$(eval $(call Profile,Minibox1))
