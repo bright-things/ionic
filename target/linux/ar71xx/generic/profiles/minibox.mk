42ad65ed63cc603af2cc5d42ae37ca21cf60cd90
Lars Boegild Thomsen <lth@netcompartner.com>
2015-04-21 10:52:44 +0800

    Missed a file

---
+++ target/linux/ar71xx/generic/profiles/minibox.mk
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2015 Bright Things UN Ltd.
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/Minibox1
+	NAME:=Minibox1
+	PACKAGES:=kmod-ath9k kmod-usb-core kmod-usb-ohci kmod-usb2 kmod-ledtrig-usbdev
+endef
+
+define Profile/Minibox1/Description
+	Package set optimized for the Minibox v1.0
+endef
+
+$(eval $(call Profile,Minibox1))
